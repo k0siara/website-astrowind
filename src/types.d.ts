82b6630424bd32143fad256a78e046942e277f7a
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2025-01-27 18:40:40 +0100

    Adds testimonials

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,7 +5,7 @@ export interface Post {
   /** A unique ID number that identifies a post. */
   id: string;
 
-  /** A post’s unique slug – part of the post’s URL based on its name, i.e. a post called “My Sample Page” has a slug “my-sample-page”. */
+  /** A post's unique slug – part of the post's URL based on its name, i.e. a post called "My Sample Page" has a slug "my-sample-page". */
   slug: string;
 
   /**  */
@@ -155,6 +155,7 @@ export interface Testimonial {
   job?: string;
   image?: string | unknown;
   course?: string;
+  rating?: number;
 }
 
 export interface Input {
