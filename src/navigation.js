90a8d2f79cb031fdef9139ecb4ff82bb966a18b0
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2025-05-24 18:02:50 +0200

    Free consultation

--- src/navigation.js
+++ src/navigation.js
@@ -65,7 +65,6 @@ export const footerData = {
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/patryk.kosieradzki' }, 
     { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: 'https://twitter.com/p_kosieradzki' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/profile.php?id=61554168906380' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
     © 2025 Patryk Kosieradzki
